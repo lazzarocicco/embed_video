f781961fb5190d5a94ce3971766cde4c8ea7ba99
marrongiallo <lazzarocicco@gmail.com>
2020-12-10 20:10:22 +0100

    12-10-20_20:10:22

--- embed_video-plugin.tcl
+++ embed_video-plugin.tcl
@@ -1,12 +1,6 @@
-# META NAME shotcanvas (shotcanvas-plugin.tcl)
-# META DESCRIPTION V02.3 load a png as patch background 
+# META NAME embed_video (embed_video-plugin.tcl)
+# META DESCRIPTION V0.1 embed a video using MPlayer in slave mode 
 # META AUTHOR <Lazzaro Ciccolella> lazzarocicco@gmail.com
-
-#    from README file
-# 1. Creates a PNG snapshot of canvas (What you see in your pd patch).
-# 2. Creates a SVG file that embed the png image you created.
-# 3. Automatically loads a png with the same name as the .pd file and places it in your patch as a background.
-#
 
 package require Tcl 8.5
 package require Tk
@@ -28,6 +22,8 @@ namespace eval E_v {
    variable video_folder "$env(HOME)/Pd/video"
    variable mplayer_pipe
    variable line
+   variable pid_player
+   variable isplayng 0
 }
 
 
@@ -36,14 +32,21 @@ proc get_done {internal_pipe finestra} {
 	if {[eof $internal_pipe]} {
 		catch {close $internal_pipe}
 		set done 1
+		set ::E_v::isplayng 0
 		::set_pipe $finestra
 		return
    	}
 	gets $internal_pipe ::E_v::line
+	set ::E_v::isplayng 1
+	if { [regexp -nocase {PAUSE} $::E_v::line] } {
+		set ::E_v::isplayng 0
+	}
+	#puts $::E_v::line
 }
 
 proc set_pipe {finestra} {
 	set ::E_v::mplayer_pipe [open "|mplayer -wid [scan [winfo id $finestra.c.f] %x] -vf crop=220:480:44:0 -slave test_video.mkv" r+]
+	set ::E_v::pid_player [pid $::E_v::mplayer_pipe]
 	fileevent $::E_v::mplayer_pipe readable [list get_done $::E_v::mplayer_pipe $finestra]
 	puts $::E_v::mplayer_pipe pause
 	flush $::E_v::mplayer_pipe
@@ -62,5 +65,13 @@ proc pause_toggle {finestra} {
 	bind $finestra <space> {puts $::E_v::mplayer_pipe pause;flush $::E_v::mplayer_pipe}
 }
 
+proc closed_window {} {
+	if {$::E_v::isplayng == 1} {
+		puts $::E_v::mplayer_pipe pause
+		flush $::E_v::mplayer_pipe
+		#exec kill $::E_v::pid_player
+	}
+}
 bind PatchWindow <<Loaded>>  {+show_player %W;pause_toggle %W}
+bind PatchWindow <Destroy>  {closed_window}
 
